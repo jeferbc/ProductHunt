f9269d21563b43a3960c063f4fdf152f14569f42
jeferbc <jeffe.bernal@gmail.com>
2017-03-13 23:27:36 -0500

    Update product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,20 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id]
-    )
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "The product was updated successfuly"
+    else
+      render :edit
+    end
   end
 
   private
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def form_title
+    @product.new_record? ? 'New Product' : 'Update Product'
+  end
 end
