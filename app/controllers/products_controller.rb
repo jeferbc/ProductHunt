b50c0ed919c0c5e0ac7f3e58e21e8738d174c728
jeferbc <jeffe.bernal@gmail.com>
2017-03-14 11:22:57 -0500

    Delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path, notice: "The product was deleted successfuly"
+
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :url, :description)
