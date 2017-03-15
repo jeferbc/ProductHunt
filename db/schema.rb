a19b7a455aa4c7f9b92e7da204a16c02aafd684c
jeferbc <jeffe.bernal@gmail.com>
2017-03-15 11:43:15 -0500

    Add users to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314164928) do
+ActiveRecord::Schema.define(version: 20170315163910) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170314164928) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
