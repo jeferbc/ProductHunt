61e7328ca256a26bb11ed314c1f133baa1b762b9
jeferbc <jeffe.bernal@gmail.com>
2017-03-17 10:47:36 -0500

    Add region to aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.paperclip_defaults = {
   storage: :s3,
-  s3_region: ENV['AWS_REGION'],
+  s3_region: ENV['us-west-1'],
   s3_credentials: {
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
